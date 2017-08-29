09a08b97aa865864ca9220dc231bfe87109eb595
shyam <m.shyam.tnj@gmail.com>
2017-08-30 00:33:44 +0530

    The multiplayer feature seems to be working...

--- s3goalma.h
+++ s3goalma.h
@@ -2,145 +2,157 @@
 #define S3GOALMA_H
 class  FILE_OPERATIONS 
 {
-	fstream  f;
 	public :
 
-	PLAYER loadPlayer(int n)
-	{
-		f.open("Player.dat", ios::binary | ios::in);
-		f.seekg(n * sizeof(PLAYER));
-		PLAYER a;
-		f.read((char*)&a, sizeof(PLAYER));
-		f.close();
-		return a;
-	}
-
-	void loadHouse (House *h, int n)
-	{
-		f.open("House.dat", ios::binary | ios::in  );
-		f.seekg(n * sizeof(House) * 30);
-		f.read((char*)h, sizeof(House) * 30);
-		f.close();
-	}
-
-	int loadGoalIndex(int n)
-	{
-		f.open("goals.dat", ios::binary | ios::in );
-		f.seekg(sizeof(n) * n);
-		f.read((char*)&n, sizeof(n));
-		f.close();
-		return n;
-	}
-
-	void loadTrees (Tree *t, int n)
-	{
-		f.open("Tree.dat", ios::binary | ios::in );
-		f.seekg(n * sizeof(Tree) * 30);
-		f.read((char*)t, sizeof(Tree) * 30);
-		f.close();
-	}
-
-	int noOfPlayers()
-	{
-		f.open("Player.dat", ios::binary | ios::in);
-		f.seekg(0, ios::end);
-		int n = f.tellg() / sizeof(PLAYER);
-		f.close();
-
-		return n;
-	}
-	void updation (PLAYER *a,House *b, Tree *c, int n)
-	{
-		f.open("Player.dat",ios::binary|ios::out|ios::in);
-		f.seekp(a->show_ID()*sizeof(PLAYER));
-		f.write((char *)a,sizeof(PLAYER));
-		f.close();
-		f.open("House.dat",ios::binary|ios::out|ios::in);
-		f.seekp(a->show_ID()*sizeof(House)*30);
-		f.write((char *)b,sizeof(House)*30);
-		f.close();
-		f.open("Tree.dat",ios::binary|ios::out|ios::in);
-		f.seekp(a->show_ID()*sizeof(Tree)*30);
-		f.write((char *)c,sizeof(Tree)*30);
-		f.close();
-		f.open("goals.dat", ios::binary | ios::in | ios::out);
-		f.seekp(a->show_ID() * sizeof(n));
-		f.write((char*)&n, sizeof(n));
-		f.close();
-	}
-	void New_Player (PLAYER *a,House *b, Tree *c)
-	{
-		//calculating the id of the player
-		f.open("Player.dat", ios::out | ios::in);
-		int id = f.tellp();
-		id = id / sizeof(PLAYER);
-		a->setid(id);
-
-		f.close();
-		f.open("Player.dat",ios::binary|ios::out|ios::app);
-		f.write((char *)a,sizeof(PLAYER));
-		f.close();
-		f.open("House.dat",ios::binary|ios::out|ios::app);
-		f.write((char *)b,sizeof(House)*30);
-		f.close();
-		f.open("Tree.dat",ios::binary|ios::out|ios::app);
-		f.write((char *)c,sizeof(Tree)*30);
-		f.close();
-		f.open("goals.dat", ios::binary | ios::app);
-		int n = 0;
-		f.write((char*)&n, sizeof(n));
-		f.close();
-	}
-	void Delete (int ID)
-	{
-		PLAYER s;
-		ofstream fout("Temp.dat",ios::binary)
-			int flag=0;
-		f.open("Player.dat", ios::in);
-		while(f.read((char*)&s,sizeof(s)))
+		PLAYER loadPlayer(int n)
 		{
-			if(ID==s.show_ID())
-				flag=1;
-			else
+			fstream f;
+			f.open("Player.dat", ios::binary | ios::in);
+			f.seekg(n * sizeof(PLAYER));
+			PLAYER a;
+			f.read((char*)&a, sizeof(PLAYER));
+			f.close();
+			return a;
+		}
+
+		void loadHouse (House *h, int n)
+		{
+			fstream f;
+			f.open("House.dat", ios::binary | ios::in  );
+			f.seekg(n * sizeof(House) * 30);
+			f.read((char*)h, sizeof(House) * 30);
+			f.close();
+		}
+
+		int loadGoalIndex(int n)
+		{
+			fstream f;
+			f.open("goals.dat", ios::binary | ios::in );
+			f.seekg(sizeof(n) * n);
+			f.read((char*)&n, sizeof(n));
+			f.close();
+			return n;
+		}
+
+		void loadTrees (Tree *t, int n)
+		{
+			fstream f;
+			f.open("Tree.dat", ios::binary | ios::in );
+			f.seekg(n * sizeof(Tree) * 30);
+			f.read((char*)t, sizeof(Tree) * 30);
+			f.close();
+		}
+
+		int noOfPlayers()
+		{
+			fstream f;
+			f.open("Player.dat", ios::binary | ios::in);
+			f.seekg(0, ios::end);
+			int n = f.tellg() / sizeof(PLAYER);
+			f.close();
+
+			return n;
+		}
+		void updation (PLAYER *a,House *b, Tree *c, int n)
+		{
+			//for troubleshooting
+			ofstream fout("ts.dat");
+			fout << a->show_ID();
+			fout.close();
+			//
+
+			fstream f;
+			f.open("Player.dat",ios::binary|ios::out|ios::in);
+			f.seekp(a->show_ID()*sizeof(PLAYER));
+			f.write((char *)a,sizeof(PLAYER));
+			f.close();
+			f.open("House.dat",ios::binary|ios::out|ios::in);
+			f.seekp(a->show_ID()*sizeof(House)*30);
+			f.write((char *)b,sizeof(House)*30);
+			f.close();
+			f.open("Tree.dat",ios::binary|ios::out|ios::in);
+			f.seekp(a->show_ID()*sizeof(Tree)*30);
+			f.write((char *)c,sizeof(Tree)*30);
+			f.close();
+			f.open("goals.dat", ios::binary | ios::in | ios::out);
+			f.seekp(a->show_ID() * sizeof(n));
+			f.write((char*)&n, sizeof(n));
+			f.close();
+		}
+		void New_Player (PLAYER *a,House *b, Tree *c)
+		{
+			//calculating the id of the player
+			fstream f;
+			f.open("Player.dat", ios::out | ios::in);
+			int id = noOfPlayers();
+			a->setid(id);
+
+			f.close();
+			f.open("Player.dat",ios::binary|ios::out|ios::app);
+			f.write((char *)a,sizeof(PLAYER));
+			f.close();
+			f.open("House.dat",ios::binary|ios::out|ios::app);
+			f.write((char *)b,sizeof(House)*30);
+			f.close();
+			f.open("Tree.dat",ios::binary|ios::out|ios::app);
+			f.write((char *)c,sizeof(Tree)*30);
+			f.close();
+			f.open("goals.dat", ios::binary | ios::app);
+			int n = 0;
+			f.write((char*)&n, sizeof(n));
+			f.close();
+		}
+		void Delete (int ID)
+		{
+			fstream f;
+			PLAYER s;
+			ofstream fout("Temp.dat",ios::binary)
+				int flag=0;
+			f.open("Player.dat", ios::in);
+			while(f.read((char*)&s,sizeof(s)))
 			{
-				if(flag==1)
-					s.setid(s.show_ID()-1);
-				fout.write((char*)&s,sizeof(s));
+				if(ID==s.show_ID())
+					flag=1;
+				else
+				{
+					if(flag==1)
+						s.setid(s.show_ID()-1);
+					fout.write((char*)&s,sizeof(s));
+				}
 			}
-		}
-		fout.close();
-		f.close();
-		remove("Player.dat");
-		rename("Temp.dat","Player.dat");
-		//House
-		House b[30];
-		f.open("House.dat", ios::in);
-		fout.open("Temp.dat",ios::binary);
-		while(f.read((char*)b,sizeof(b)))
-		{
-			if(ID!=s.show_ID())
-				fout.write((char*)b,sizeof(b));
-		}
-		fout.close();
-		f.close();
-		remove("House.dat");
-		rename("Temp.dat","House.dat");
-
-		//Tree
-
-		Tree c[30];
-		f.open("tree.dat", ios::in);
-		fout.open("Temp.dat",ios::binary)
-			while(f_t.read((char*)c,sizeof(c)))
+			fout.close();
+			f.close();
+			remove("Player.dat");
+			rename("Temp.dat","Player.dat");
+			//House
+			House b[30];
+			f.open("House.dat", ios::in);
+			fout.open("Temp.dat",ios::binary);
+			while(f.read((char*)b,sizeof(b)))
 			{
 				if(ID!=s.show_ID())
-					fout.write((char*)c,sizeof(c));
+					fout.write((char*)b,sizeof(b));
 			}
-		fout.close();
-		f.close();
-		remove("Tree.dat");
-		rename("Temp.dat","Tree.dat");
-	}
+			fout.close();
+			f.close();
+			remove("House.dat");
+			rename("Temp.dat","House.dat");
+
+			//Tree
+
+			Tree c[30];
+			f.open("tree.dat", ios::in);
+			fout.open("Temp.dat",ios::binary)
+				while(f_t.read((char*)c,sizeof(c)))
+				{
+					if(ID!=s.show_ID())
+						fout.write((char*)c,sizeof(c));
+				}
+			fout.close();
+			f.close();
+			remove("Tree.dat");
+			rename("Temp.dat","Tree.dat");
+		}
 
 } fileop;
 
