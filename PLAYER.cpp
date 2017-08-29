716af2d1a9761a9915bf8130d56ce3cdf1f82ada
shyam <m.shyam.tnj@gmail.com>
2017-08-29 23:44:57 +0530

    PLAYER.CPP only appends a default record to PLAYER.DAT

--- PLAYER.cpp
+++ PLAYER.cpp
@@ -21,18 +21,17 @@ void main()
 {
 	clrscr();
 	fstream f;	
-	PLAYER a ;
+	PLAYER a  = {10,10,10};
+	strcpy(a.userName, "shyam");
+	a.money = 1;
+	a.wood = 2;
+	a.water = 3;
+	a.produce = 4;
+	a.maxXp = 10;
+	a.index = 10;
 
-	f.open("player.dat", ios::binary|ios::in);
-	while(f.read((char*)&a, sizeof(a)))
-	{
-		puts(a.userName);
-		cout << a.produce << endl;
-		cout << a.water << endl;
-		cout << a.money << endl;
-		cout << a.wood << endl;
-		cout << a.id << endl;
-		cout << endl;
-		getch();
-	}
+	f.open("player.dat", ios::binary|ios::app);
+	f.write((char*)&a, sizeof(a));
+	f.close();
+
 }
