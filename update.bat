050bd3e539987e5369816b4c3b2bdd6cfaf3cdcb
Shyam <m.shyam.tnj@gmail.com>
2017-08-30 23:29:06 +0530

    Edited update.bat also for the new folder

--- update.bat
+++ update.bat
@@ -1,8 +1,8 @@
 @echo off
 copy /Y s3* ..\
 copy /y GOALDATA.DAT ..\
-copy /y writegoals.cpp ..\
+copy /y additionalToolsSource\writegoals.cpp ..\
 copy /y s3goalma.h ..\
-copy /y PLAYER.CPP ..\
-copy /y playread.CPP ..\
+copy /y additionalToolsSource\PLAYER.CPP ..\
+copy /y additionalToolsSource\playread.CPP ..\
 pause
