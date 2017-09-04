0e81017c4bc03657528f41300279a464480b2779
shyam <m.shyam.tnj@gmail.com>
2017-09-04 07:07:41 +0530

    Checked with windows. Removed update.bat and delbin.bat because it seemed more logical.

--- delbin.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-del ..\player.dat
-del ..\goals.dat
-del ..\house.dat
-del ..\tree.dat
-pause
--- update.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-copy /Y s3* ..\
-copy /y GOALDATA.DAT ..\
-copy /y additionalToolsSource\writegoals.cpp ..\
-copy /y s3goalma.h ..\
-copy /y additionalToolsSource\PLAYER.CPP ..\
-copy /y additionalToolsSource\playread.CPP ..\
-pause
